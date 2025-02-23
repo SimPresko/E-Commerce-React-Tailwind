aa2aae8a975393f00f027f3e2d7897ba0cd20431
SimPresko <preslibankov@gmail.com>
2025-02-23 16:54:20 +0100

    Version 2 - Add sizes for the product - Add colors and sizes button transitions on hover - Implement add to cart button and add tap transition with framer-motion module - Add image transitions when changing color and more with framer-motion module

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { motion } from 'framer-motion'
 
 const product = {
   name: 'Nike Air Max 270',
@@ -18,22 +19,56 @@ const ProductPage = () => {
 
   return (
     <div className='gradient-bg flex justify-center items-center'>
-      <div className='bg-sky-900 p-6 rounded-lg shadow-xl max-w-2xl'>
-        <img className='h-100' src={product.image[selectedColor]} />
+      <div className='bg-sky-900 p-6 rounded-lg min-h-[300px] shadow-xl min-w-sm max-w-2xl h-140'>
+        <motion.img
+          key={selectedColor}
+          src={product.image[selectedColor]}
+          alt={product.name}
+          className='w-full h-75 object-cover rounded-lg duration-150'
+          transition={{ duration: 0.5 }}
+          initial={{ opacity: 0.1, scale: 0.9 }}
+          animate={{ opacity: 1, scale: 1 }}
+          whileHover={{ scale: 1.1 }}
+        />
         <h2 className='text-2xl font-bold mt-4 text-sky-200'>{product.name}</h2>
-        <p className='text-lg text-sky-300 pb-4'>{product.price}</p>
-        <div className='mt-4 flex space-x-2'>
+        <p className='text-lg text-sky-300 pb-2'>{product.price}</p>
+        <div className='my-4 flex space-x-2 h-8'>
           {product.colors.map((color) => (
             <button
               key={color}
               className={`w-15 h-6 rounded-full ${
-                selectedColor === color ? `ring-3 ring-black` : ''
+                selectedColor === color
+                  ? `ring-1 ring-white opacity-100`
+                  : 'hover:scale-110 duration-200 opacity-50 hover:opacity-100 cursor-pointer'
               }`}
               style={{ backgroundColor: color }}
               onClick={() => setSelectedColor(color)}
             />
           ))}
         </div>
+        <div className='my-4 py-4 space-x-2 flex'>
+          {product.sizes.map((size) => (
+            <button
+              key={size}
+              className={` h-10 w-10 border rounded ${
+                selectedSize === size
+                  ? 'bg-black text-white'
+                  : 'cursor-pointer hover:scale-110 duration-200'
+              }`}
+              onClick={() => setSelectedSize(size)}
+            >
+              {size}
+            </button>
+          ))}
+        </div>
+
+        <motion.button
+          className='mt-4 w-full bg-black text-white py-2 rounded-lg cursor-pointer duration-200'
+          whileTap={{ scale: 0.9 }}
+          whileHover={{ scale: 1.05 }}
+        >
+          Add to Cart
+        </motion.button>
       </div>
     </div>
   )
